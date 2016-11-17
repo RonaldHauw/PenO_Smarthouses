a68633816bcf241d92cbbac38e441721eb809d07
Arne Lambeets <arnelambeets@MacBook-Pro-van-Arne.local>
2016-11-17 17:00:22 +0100

    Demopagina's mooi in orde

--- Demopage/urls.py
+++ Demopage/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.Demo_homepage, name='Demo_homepage'),
     url(r'^Uitleg',views.demo_uitleg,name='Demo_uitleg'),
-    url(r'^contact',views.contact,name='Contact_page')
+    url(r'^contact',views.contact,name='Contact_page'),
+    url(r'^root',views.Root,name='Root')
 
 ]
 
