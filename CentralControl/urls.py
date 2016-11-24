3baf7f22bde817bbe59ac312e9f7a2cf7c825296
ronnepon <ronald@MacBook-Pro.local>
2016-11-24 18:09:54 +0100

    viktorkutcomputer

--- CentralControl/urls.py
+++ CentralControl/urls.py
@@ -1,19 +1,19 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
 from . import views
-from . import aux
+from . import auxilary
 
 
 urlpatterns = [
 
 url(r'^$',views.indexcentralcontrol, name='IndexCentralControl'),
 url(r'^testinterface',views.testinterface, name='testinterface'),
-url(r'^ledaan',aux.ledaan, name='ledaan'),
-url(r'^leduit',aux.leduit, name='leduit'),
-url(r'^led2aan',aux.led2aan, name='led2aan'),
-url(r'^led2uit',aux.led2uit, name='led2uit'),
-url(r'^led2fel',aux.led2fel, name='led2fel'),
-url(r'^led2zwak',aux.led2zwak, name='led2zwak'),
+url(r'^ledaan', auxilary.ledaan, name='ledaan'),
+url(r'^leduit', auxilary.leduit, name='leduit'),
+url(r'^led2aan', auxilary.led2aan, name='led2aan'),
+url(r'^led2uit', auxilary.led2uit, name='led2uit'),
+url(r'^led2fel', auxilary.led2fel, name='led2fel'),
+url(r'^led2zwak', auxilary.led2zwak, name='led2zwak'),
 
 
 url(r'^doityourself',views.handmatig, name='handmatig'),
