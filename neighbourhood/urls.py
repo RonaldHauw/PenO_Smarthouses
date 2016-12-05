7966db1323a1f7c3d8c2660c838f5f4129f49b27
Ronald <Pauline@MacBook-Pro-van-Pauline.local>
2016-12-05 20:33:02 +0100

    werkende grafieken toegevoegd

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -5,6 +5,7 @@ import auxilary
 
 urlpatterns = [
     url(r'^House/(?P<house_id>[0-9]+)$',views.house, name='House'),
+    url(r'^House/(?P<house_id>[0-9]+)/chart_data_energy$',views.getchartdata,name='Chartdata'),
     url(r'^room/(?P<id>[0-9]+)$',views.room, name='Room'),
     url(r'^$',views.indexneighbourhood, name='IndexNeighbourhood'),
     url(r'^CentralControl',views.centralcontrol,name="ToCentralControl"),
@@ -16,6 +17,7 @@ urlpatterns = [
     url(r'^status/(?P<appliance_id>[0-9]+)/(?P<parametertochange>[0-9]+)/(?P<value>[0-9]+)$', auxilary.change_status, name='status'),
 
     url(r'^doityourself',views.handmatig, name='handmatig'),
+    url(r'^optimalisated',views.vergelijking, name = 'vergelijking'),
 
 
 ]
--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from models import *
 from django.shortcuts import render
@@ -10,8 +10,8 @@ from django.template.response import TemplateResponse
 def house(request,house_id):
 
     template = loader.get_template('neighbourhood/house.html')
-    # context = {'house': House.objects.get(ip_address=house_id)}
-    context = {'house': house_id}
+    context = {'house': House.objects.get(id=house_id)}
+    #context = {'house': house_id}
     return TemplateResponse(request,template,context)
 
 def room(request,id):
@@ -65,4 +65,48 @@ def testinterface(request):
 
 def handmatig(request):
     template = loader.get_template('Handmatig.html')
+    return HttpResponse(template.render(request))
+
+def getchartdata(request,house_id):
+    house = House.objects.get(id=house_id)
+    #values = house.get_total_energy()
+    data = dict()
+    data['timeseries'] = {
+                "x": "2013-02-08 09:30",
+                "y": 0
+            }, {
+                "x": "2013-02-08 09:45"  ,
+                "y": 5
+            }, {
+                "x": "2013-02-08 10:00"  ,
+                "y": 10
+            }, {
+                "x": "2013-02-08 10:15"  ,
+                "y": 7
+            }
+    return JsonResponse(data)
+
+
+def getallcosts(request,house_id):
+    house = House.objects.get(id=house_id)
+    #values = house.get_total_energy()
+    data = dict()
+    data['timeseries'] = {
+                "x": "2013-02-08 09:33",
+                "y": 3
+            }, {
+                "x": "2013-02-08 09:45"  ,
+                "y": 7
+            }, {
+                "x": "2013-02-08 09:51"  ,
+                "y": 10
+            }, {
+                "x": "2013-02-08 10:55"  ,
+                "y": 5
+            }
+    return JsonResponse(data)
+
+
+def vergelijking(request):
+    template = loader.get_template('neighbourhood/vergelijking.html')
     return HttpResponse(template.render(request))
